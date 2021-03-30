dbfb55634d774405e69fc9a1d825823a0450d710
larissamiguel <74651820+LarissaMiguelTsatedem@users.noreply.github.com>
2021-03-30 13:34:46 +0200

    fin microService

---
+++ Microservice2Application/src/main/java/com/example/Microservice2Application/Microservice2Application.java
@@ -0,0 +1,15 @@
+package com.example.Microservice2Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class Microservice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice2Application.class, args);
+	}
+
+}
